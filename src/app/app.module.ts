3d582f6564fa2f951b5de3124b8d835eaf9e117a
Javier Choque <jdka.suburbio@gmail.com>
2023-04-28 08:57:01 -0400

    Angular Project

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './commons/menu/menu.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
