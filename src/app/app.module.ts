7eeea891ab0cebe41eab6d0da46ed95d8ef19165
Fernando-MV7 <huviel.fer@gmail.com>
2023-05-01 11:52:06 -0400

    base de datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,31 @@ import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './commons/menu/menu.component';
+import { BasededatosComponent } from './basededatos/basededatos.component';
+
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatMenuModule} from '@angular/material/menu';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    BasededatosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatInputModule,
+    MatBadgeModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
