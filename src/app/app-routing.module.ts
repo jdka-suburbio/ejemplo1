7f35d278b1af4f249cad901d0bd2d78906485a70
Sis414Dani <you@example.com>
2023-05-05 11:01:05 -0400

    prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProgramacionComponent } from './programacion/programacion.component';
+import { IngleComponent } from './video/ingle/ingle.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
-  {path: "programacion", component:ProgramacionComponent}
+  {path: "programacion", component:ProgramacionComponent},
+  {path: "ingles", component:IngleComponent}
 ];
 
 @NgModule({
---
+++ src/app/video/ingle/ingle.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingle',
+  templateUrl: './ingle.component.html',
+  styleUrls: ['./ingle.component.css']
+})
+export class IngleComponent {
+
+}
