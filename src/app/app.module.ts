765b20afae93e5634788795f26fac7b0f69093da
Javier Ch <jdka.suburbio@gmail.com>
2023-05-17 18:15:55 -0400

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { ProgramacionComponent } from './programacion/programacion.component';
 import { CuestionarioComponent } from './cuestionario/cuestionario.component';
 import { IncioComponent } from './incio/incio.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -25,7 +29,10 @@ import { IncioComponent } from './incio/incio.component';
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
