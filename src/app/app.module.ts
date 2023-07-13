57c693b83ffd5285a1e63885ae863446075ea71a
Sis414Dani <you@example.com>
2023-07-13 10:42:05 -0400

    ingli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,9 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './commons/menu/menu.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { ProgramacionComponent } from './programacion/programacion.component';
-import { CuestionarioComponent } from './cuestionario/cuestionario.component';
 import { IngleComponent } from './video/ingle/ingle.component';
 import { IngliComponent } from './ingli/ingli.component';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
@@ -36,10 +33,7 @@ import { RegisterComponent } from './register/register.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     MenuComponent,
-    ProgramacionComponent,
-    CuestionarioComponent,
     IngleComponent,
     IngliComponent,
     KeysPipe,
